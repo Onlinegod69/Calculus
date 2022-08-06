f9ab87c905970da1540770eec76cbf0137449360
Onlinegod69 <110566241+Onlinegod@users.noreply.github.com>
2022-08-06 15:46:37 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+def main(firstNumber, SecondNumber):
+  def multiply (a, b):
+     return (a * b)
+
+  print multiply(firstNumber,SecondNumber)
+
+  def summ(a, b):
+    return a + b
+
+  print summ(firstNumber,SecondNumber)
+  def minus(a,b):
+    return a - b
+  print minus(firstNumber,SecondNumber)
+  def div(a, b):
+    return a/ b
+  print div(firstNumber,SecondNumber)
+
+
+main(10.0, 3)
